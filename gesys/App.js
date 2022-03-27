63c8a6b55d8e3dd69f501a00004a13964f965c55
Abdullah <aabdullahsmx2014@gmail.com>
2022-03-27 12:40:48 +0200

    Menu lateral, top bar de ejemplo

--- gesys/App.js
+++ gesys/App.js
@@ -1,13 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View, Alert } from 'react-native';
+import LateralMenu from './LateralMenu';
 import Boton from './Boton';
+import React from 'react';
+import { Button } from 'react-native-paper';
+import { WebView } from 'react-native';
+import TopBar from './TopBar';
 
-export default function App() {
-  return (
-    <Boton />
-  );
+export default class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {isHidden: false};
+    this.onPress = this.onPress.bind(this);
+  }
+
+  onPress() {
+    this.setState({isHidden: !this.state.isHidden})
+  }
+
+  render() {
+    return (
+      <ScrollView>
+        <TopBar></TopBar>
+        <Button
+        icon="camera" 
+        mode="contained" 
+        onPress={() => this.onPress()} 
+        >
+        Press me
+        </Button>
+         <LateralMenu></LateralMenu>
+      </ScrollView>
+      
+    );
+  }
+
 }
-
 
 const styles = StyleSheet.create({
   container: {
