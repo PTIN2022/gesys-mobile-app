b9009d739ba8ac7f1ff3e026f51a97521e0f9b28
Abdullah <aabdullahsmx2014@gmail.com>
2022-04-10 19:26:41 +0200

    Define only add action for vehicle, booking and stations

--- VehicleActions.js
+++ VehicleActions.js
@@ -2,7 +2,7 @@
 // Acción para añadir el vehículo a la store.
 export const addVehicle = vehicle => (
     {
-      type: 'ADD_VEHICE',
+      type: 'ADD_VEHICLE',
       payload: vehicle,
     }
 );
