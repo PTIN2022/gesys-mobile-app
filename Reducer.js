405f0b1619e9effda848a985affb5d91c1843d4e
Abdullah <aabdullahsmx2014@gmail.com>
2022-04-10 19:41:58 +0200

    Add action perfomers

--- Reducer.js
+++ Reducer.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 
 const INITIAL_STATE = {
   current: [],
-  all_vehicle: [ // Guardamos los vehículos.
+  all_vehicles: [ // Guardamos los vehículos.
   ],
   all_bookings: [ // Guardamos las reservas.
   ],
@@ -12,9 +12,34 @@ const INITIAL_STATE = {
 };
 
 const gesysReducer = (state = INITIAL_STATE, action) => {
+  const {
+    current
+  } = state;
+
   switch (action.type) {
+    case 'ADD_STATION': // Definimos lo que hacemos con la acción.
+      const { all_stations } = state;
+      // Insertamos en la primera posición.
+      const addedStation = all_stations.splice(action.payload, 1);
+      current.push(addedStation);
+      return { current, all_stations };
+
+    case 'ADD_VEHICLE': // Definimos lo que hacemos con la acción.
+      const { all_vehicles } = state;
+      // Insertamos en la primera posición.
+      const addedVehicle = all_vehicles.splice(action.payload, 1);
+      current.push(addedVehicle);
+      return { current, all_vehicles };
+
+    case 'ADD_BOOKING': // Definimos lo que hacemos con la acción.
+      const { all_bookings } = state;
+      // Insertamos en la primera posición.
+      const addedBooking = all_bookings.splice(action.payload, 1);
+      current.push(addedBooking);
+      return { current, all_bookings };
+
     default:
-    return state
+      return state
   }
 };
 
