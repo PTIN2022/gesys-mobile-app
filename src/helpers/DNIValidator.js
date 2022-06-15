6e8b9b74fb7eb553de0283e0e7796a4949ebd0eb
maf29 <73944513+maf29@users.noreply.github.com>
2022-06-15 12:31:19 +0200

    validator DNI y phone

--- src/helpers/DNIValidator.js
+++ src/helpers/DNIValidator.js
@@ -1,5 +1,5 @@
 //Esta función es un helper que comprueba si el dni que ha introducido el usuario es válido.
-export function matriculaValidator(DNI) {
+export function DNIValidator(DNI) {
   const re = /^([0-9]){8}([A-Z]){1}$/
   if (!DNI) return "El campo DNI no puede estar vacío!"
   if (!re.test(DNI)) return 'Ooops! Necesitamos un DNI válido.'
--- src/helpers/phoneValidator.js
+++ src/helpers/phoneValidator.js
@@ -1,5 +1,5 @@
 //Esta función es un helper que comprueba si el numero de telefono que ha introducido el usuario es válido.
-export function matriculaValidator(phone) {
+export function phoneValidator(phone) {
   const re = /^([0-9]){9}$/
   if (!phone) return "El campo teléfono no puede estar vacío!"
   if (!re.test(phone)) return 'Ooops! Necesitamos un numeor de teléfono válido.'
