15451c6601142579a3b8368b2041e7c2af7ea59d
maf29 <73944513+maf29@users.noreply.github.com>
2022-06-15 17:23:12 +0200

    NIe add

--- src/helpers/DNIValidator.js
+++ src/helpers/DNIValidator.js
@@ -1,7 +1,8 @@
 //Esta función es un helper que comprueba si el dni que ha introducido el usuario es válido.
 export function DNIValidator(DNI) {
-  const re = /^([0-9]){8}([A-Z]){1}$/
+  const re = /^([0-9]){8}([A-Z]){1}$/   //DNI
+  const re2 = /^([X-Z]){1}([0-9]){7}([A-Z]){1}$/    //NIE
   if (!DNI) return "El campo DNI no puede estar vacío!"
-  if (!re.test(DNI)) return 'Ooops! Necesitamos un DNI válido.'
+  if (!re.test(DNI) || !re2.test(DNI)) return 'Ooops! Necesitamos un DNI válido.'
   return ''
 }
