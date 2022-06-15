1c1cbdea7fca368e1a0871df0b6eef51d267d504
Abdullah <aabdullahsmx2014@gmail.com>
2022-06-15 17:40:49 +0200

    Transacciones modificada

--- src/views/Transactions.js
+++ src/views/Transactions.js
@@ -1,6 +1,7 @@
 import React, { Component, useState } from 'react';
 import { connect } from 'react-redux';
-import { Text } from 'react-native'
+import { Image, View, StyleSheet, Text, ActivityIndicator } from 'react-native'
+import { Button, Paragraph, Dialog, Portal, Snackbar, Divider, Card, TextInput, RadioButton } from 'react-native-paper';
 
 
 class Transactions extends React.Component {
@@ -9,16 +10,29 @@ class Transactions extends React.Component {
     }
 
     componentDidMount() {
-		if(this.props.Login.logged === false){
-			this.props.navigation.navigate('LogIn');
-		}    
+      if(this.props.Login.logged === false){
+        this.props.navigation.navigate('LogIn');
+      }    
     }
 
     render() {
-        return this.props.Transactions.transaction.length > 0 ? 
-            <Text>Transacciones encontradas. Tienes 0 puntos.</Text>
-            : 
-            <Text>No se ha encontrado ningún historial de transacciones. Tienes 0 puntos.</Text>
+      return (
+        <View>
+          <View style={{alignItems: 'center', marginTop: 20, backgroundColor: '#0E3BAC', justifyContent: 'center'}} height={100}>
+            <Text style={{textAlignVertical: 'center', fontSize: 50, color: 'white'}}>
+              0
+              <Text style={{textAlignVertical: 'center', fontSize: 15, color: 'white'}}> puntos</Text>
+            </Text>
+          </View>
+          <View style={{marginTop: 10}}>
+            <Text>No tiene ningún historial de transacciones por el momento.</Text>
+          </View>
+        </View>
+      )
+        // return this.props.Transactions.transaction.length > 0 ? 
+        //     <Text>Transacciones encontradas. Tienes 0 puntos.</Text>
+        //     : 
+        //     <Text>No se ha encontrado ningún historial de transacciones. Tienes 0 puntos.</Text>
     }
 }
 
